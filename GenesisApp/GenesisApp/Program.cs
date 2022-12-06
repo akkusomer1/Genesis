249d27d852bd7c914e9f9d91a671ff3e41373a51
akkusomer1 <118826338+akkusomer1@users.noreply.github.com>
2022-12-06 13:01:50 +0300

    Güncellendi

--- GenesisApp/GenesisApp/Program.cs
+++ GenesisApp/GenesisApp/Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Hello, World!");
-
 Console.WriteLine("Hello");
-=======
+
 Console.WriteLine("Merhaba, Türkiye!");
->>>>>>> refs/remotes/origin/main
+
+Console.WriteLine("Merhaba, Turkey!");
+
